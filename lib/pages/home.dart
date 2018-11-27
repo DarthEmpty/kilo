2ba735a93ecb6d71d380020c3fd66cea830d1637
Joel Clarke <joel.clarke273@gmail.com>
2018-11-27 19:00:15 +0000

    Create Home page

---
+++ lib/pages/home.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text("Account Box"),
+              subtitle: Text("Can't get enough of 'em"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.phone_android),
+              title: Text("Phone"),
+              subtitle: Text("Thanks, Alexander Grand Bell!"),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.bookmark),
+              title: Text("Bookmark"),
+              subtitle: Text("Let's not forget this..."),
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
