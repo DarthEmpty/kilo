f59c096f875137e972240416c937e91441e9828d
Joel Clarke <joel.clarke273@gmail.com>
2018-12-22 23:35:29 +0000

    Create SessionForm page

---
+++ lib/pages/session_form.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+
+class SessionForm extends StatefulWidget {
+  @override
+  _SessionFormState createState() => _SessionFormState();
+}
+
+class _SessionFormState extends State<SessionForm> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
